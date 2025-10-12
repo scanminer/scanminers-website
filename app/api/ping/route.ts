638daa54f1e3333c9df9a4766bbe98c62a375a0a
mahmood <nikan3nikan2nikan@@gmail.com>
2025-10-12 03:34:10 +0300

    chore(api): add /api/ping minimal edge route for prod isolation

---
+++ app/api/ping/route.ts
@@ -0,0 +1,6 @@
+export const runtime = 'edge'
+export const dynamic = 'force-dynamic'
+
+export function GET() {
+  return new Response('pong', { status: 200, headers: { 'content-type': 'text/plain' } })
+}
