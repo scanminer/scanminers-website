932b1ca9639825d49430ef6a0a3fc94899b1cfa5
mahmood <nikan3nikan2nikan@@gmail.com>
2025-10-12 04:08:10 +0300

    chore(api): remove /api/health/env (replaced by /api/env); avoid next-on-pages duplicate identifier issue

---
+++ .cf-nop-2/_next/static/chunks/833.d5f448faf7950fa1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[833],{214:e=>{function r(e){return Promise.resolve().then(()=>{var r=Error("Cannot find module '"+e+"'");throw r.code="MODULE_NOT_FOUND",r})}r.keys=()=>[],r.resolve=r,r.id=214,e.exports=r}}]);
